c6ac5ce7c370a07c9488f7cb49d81ac3cdf29940
Harshinin P <harshini11122003@gmail.com>
2025-01-04 20:33:35 +0530

    Add files via upload

---
+++ ImportandExport.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+const ImportandExport=()=>
+{
+    return(
+        <div>
+            <h1>This is child1 file of Import and Export</h1>
+        </div>
+    )
+}
+const ImportandExportAno=()=>
+    {
+        return(
+            <div>
+                <h1>This is child2 file of Import and Export</h1>
+            </div>
+        )
+    }
+    // export default ImportandExport;-Default export
+    // export default ImportandExportAno;-Default export
+// export {ImportandExport,ImportandExportAno};-Named Export
---
+++ ImportandExport1.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+// import ImportandExport from './ImportandExport'-Default Export
+// import {ImportandExport,ImportandExportAno} from "./ImportandExport"-Named export
+const ImportandExport1 = () => {
+  return (
+    <div>
+        <h1>This is main file of Import and Export</h1>
+        <ImportandExport/>
+        <ImportandExportAno/>
+        </div>
+  )
+}
+
+export default ImportandExport1;
