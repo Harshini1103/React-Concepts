3cadeefe7bee20b0c8d6c1e6250c23c0e8941bd5
Harshinin P <harshini11122003@gmail.com>
2025-01-04 21:20:27 +0530

    Add files via upload

---
+++ Fragments.jsx
@@ -0,0 +1,29 @@
+import React, { Fragment } from 'react';
+// const Fragments=()=>
+// {
+// return(
+// <>
+// <h1>Hello World</h1>
+// <p>This is a paragraph</p>
+// </>
+// )
+// }with Empty Fragments
+// const Fragments=()=>
+//     {
+//     return(
+//     <div>
+//     <h1>Hello World</h1>
+//     <p>This is a paragraph</p>
+//     </div>
+//     )
+//     }//without Fragment
+const Fragments=()=>
+        {
+        return(
+        <Fragment>
+        <h1>Hello World</h1>
+        <p>This is a paragraph</p>
+        </Fragment>
+        )
+        }
+export default Fragments;
