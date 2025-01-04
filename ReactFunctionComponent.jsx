8a553775ad38f480e3979f84d935226ac8b90c2f
Harshinin P <harshini11122003@gmail.com>
2025-01-04 18:54:41 +0530

    Add files via upload

---
+++ ReactFunctionComponent.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+const ReactFunctionComponent=()=>{
+return(
+    <div>
+        <h1>Hello,This Is React.js</h1>
+        <p>React.js is an Open Source Javascript Library,used for building User Interfaces(UI) and primarily used for Single Page Application.Developed and Maintained By Facebook and a large developer Community. </p>
+    </div>
+)
+}
+export default ReactFunctionComponent;
